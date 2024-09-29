ae37c51c2cd795c8bdaeb9bd566ca2f6f8015deb
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2024-09-28 18:31:12 -0600

    Refactor CardIsInView component to fix isInView behavior

--- svelte/src/components/CardIsInView.svelte
+++ svelte/src/components/CardIsInView.svelte
@@ -1,44 +1,32 @@
 <script>
     import {fade} from 'svelte/transition';
     import { inview } from 'svelte-inview';
-    let isInView;
+
+    let isInView = false;
 </script>
+<article use:inview={{ unobserveOnEnter: true, rootMargin: '-20%' }}
+on:inview_change={({ detail }) => {
+    isInView = detail.inView;
+    console.log("isInView",isInView);
+}}>
+    {#if isInView && $$slots.header}
+    <header in:fade>
+        <slot name="header"></slot>
+    </header>
+    {/if}
 
-    <!-- <div use:inview={{ unobserveOnEnter: true, rootMargin: '-20%' }} -->
-    <!-- on:inview_change={({ detail }) => { -->
-        <!-- isInView = detail.inView; -->
-        <!-- console.log("isInView",isInView); -->
-    <!-- }}> -->
+    {#if isInView && $$slots.body}
+    <body in:fade={{ delay: 250, duration: 2000 }} >
+        <slot name="body"></slot>
+    </body>
+    {/if}
 
-        <!-- {#if isInView} -->
-        <article use:inview={{ unobserveOnEnter: true, rootMargin: '-20%' }}
-        on:inview_change={({ detail }) => {
-            isInView = detail.inView;
-            console.log("isInView",isInView);
-        }}>
-            {#if isInView && $$slots.header}
-            <header in:fade>
-                <slot name="header"></slot>
-            </header>
-            {/if}
-
-            {#if isInView && $$slots.body}
-            <body in:fade={{ delay: 250, duration: 2000 }}>
-                <slot name="body"></slot>
-            </body>
-            {/if}
-
-            {#if isInView && $$slots.footer}
-            <footer in:fade>
-                <slot name="footer"></slot>
-            </footer>
-            {/if}
-        </article>
-        <!-- {/if} -->
-    <!-- </div> -->
+    {#if isInView && $$slots.footer}
+    <footer in:fade>
+        <slot name="footer"></slot>
+    </footer>
+    {/if}
+</article>
 
 <style>
-    /* .wrapper { */
-        /* margin-top: 30px; */
-    /* } */
 </style>
